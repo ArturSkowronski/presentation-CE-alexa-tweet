b76d4976175d57af35a85aec3fe5cb4110b0c5eb
Artur Skowronski <a.skowronski@smartrecruiters.com>
2017-02-08 22:30:55 +0100

    6. Use basic SSML tags and output clearance

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 var twitter = require("./src/twitter-client");
 var Alexa = require('alexa-sdk');
 
+var clearTag = function(value){
+	value = value.replace(/meetjs/ig, "Meet JS")
+	value = value.replace(/#/g, '')
+	value = value.replace(/@/g, '')
+	return value;
+} 
+
 var handlers = {
 	'LaunchRequest': function () {
 		twitter
 		.getByHash('meetjs')
 		.then((tweets) => {
-			this.emit(":tell", `${tweets.statuses[0].text} by ${tweets.statuses[0].user.name}`);
+			this.emit(":tell", clearTag(`${tweets.statuses[0].text} <break time="1s"/> by ${tweets.statuses[0].user.name}`));
 		});
 	}
 };
